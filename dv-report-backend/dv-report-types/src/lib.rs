f3feba01e76d4334028216f6544f218996b80596
kukabi <kutsalbilgin@gmail.com>
2025-05-25 15:22:48 +0300

    Further fix of multi-chain feature.

--- dv-report-backend/dv-report-types/src/lib.rs
+++ dv-report-backend/dv-report-types/src/lib.rs
@@ -5,7 +5,7 @@ compile_error!("You must enable only one of the features: polkadot or kusama.");
 #[cfg(feature = "kusama")]
 pub use metadata::kusama as runtime;
 #[cfg(feature = "kusama")]
-pub use metadata::kusama::api::runtime_types::kusama_runtime::RuntimeCall;
+pub use metadata::kusama::api::runtime_types::staging_kusama_runtime::RuntimeCall;
 #[cfg(feature = "polkadot")]
 pub use metadata::polkadot as runtime;
 #[cfg(feature = "polkadot")]
