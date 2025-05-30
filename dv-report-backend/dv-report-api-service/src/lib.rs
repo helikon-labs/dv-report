0bdc695c1dbab4471316c10f8da89a8327c7c5c8
kukabi <kutsalbilgin@gmail.com>
2025-05-30 17:04:49 +0300

    Remove CORS from API.

--- dv-report-backend/dv-report-api-service/src/lib.rs
+++ dv-report-backend/dv-report-api-service/src/lib.rs
@@ -49,7 +49,7 @@ impl Service for APIService {
             CONFIG.api.api_service_port
         );
         let server = HttpServer::new(move || {
-            let cors = Cors::default()
+            let _cors = Cors::default()
                 .allowed_origin("http://localhost:8080")
                 .allowed_methods(vec!["GET", "POST", "OPTIONS"])
                 .allowed_headers(vec![
@@ -62,7 +62,7 @@ impl Service for APIService {
                 .app_data(web::Data::new(ServiceState {
                     postgres: postgres.clone(),
                 }))
-                .wrap(cors)
+                //.wrap(cors)
                 .wrap_fn(|request, service| {
                     metrics::request_counter().inc();
                     metrics::connection_count().inc();
